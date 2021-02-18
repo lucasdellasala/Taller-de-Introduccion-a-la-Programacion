4357586614aecc7559d47d5c3a0d059bc0a49046
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2021-02-18 19:46:58 -0300

    first commit

---
+++ clase1/tipos.js
@@ -0,0 +1,38 @@
+//Boolean: buleanos
+let mayorDeEdad = false;
+mayorDeEdad = true;
+
+//Number: números reales
+let numero = 1;
+let numeroDecimal = 6.25;
+let numeroNegativo = -4.57;
+let edad = 24;
+
+//En caso de ser números que nunca van a cambiar se utiliza const para declarar la variable
+const PI = 3.14;
+const R = 0.082;
+const G = 6.674e-11;
+
+//String: cadena de caracteres
+let nombre = "Lucas";
+let oracion = "Lucas estaba 'insssssssta'.";
+let nacionalidad = "Argentina";
+let stringVacio = "";
+
+//Array: listas
+let jugadores = ["Messi", "Cristiano", "Lewandowski"];
+let paginas = [1,2,3,4,5,6,7];
+let listaTiposMezclados = [1,"Messi",true,3.14,false,"hola"];
+
+//Objects: objectos
+let estudiante = {
+    nombre : "Lucas",
+    edad : 24,
+    universidad: "UBA"
+};
+
+//Undefined y null
+let variableIndefinida;
+let variableNula = null;
+
+//console.log("HOLAAAAAAAAAAAAAA")
---
+++ clase2/logica.js
@@ -0,0 +1,80 @@
+//Suma
+let x = 5 + 2
+//Resta
+let y = 5 - 2
+//Multi
+let z = 5 * 2
+//Division
+let a = 5 / 2
+//Resto => modulo
+let b = 5 % 2
+//Potencia
+let c = 5**2
+
+let d = x + y
+
+let e = "5" + 5
+let f = 55
+
+//Comparaciones
+let g = e==f // no estricto
+let h = e===f // estricto
+let i = (e!=f) // distinto
+
+let todasLasMateriasAp = true;
+let promedioIgualOMayorA7 = false;
+
+let teRegaloElCelu = todasLasMateriasAp && promedioIgualOMayorA7 // AND
+let algunaDeLasDos = todasLasMateriasAp || promedioIgualOMayorA7 // OR
+
+let aDiciembre = !todasLasMateriasAp //contrario (negacion)
+
+
+//If
+let teQuedaronMasDe2 = false
+
+if (teQuedaronMasDe2) {
+    console.log("Repetis")
+} else {
+    console.log("No repetis")
+}
+
+//Elseif
+let cantMaterias = 0
+
+if (cantMaterias===0) {
+    console.log("Pasas limpio")
+} else if (cantMaterias>2) {
+    console.log("Repetis")
+} else {
+    console.log("Previa")
+}
+
+
+//ShorthandOperator
+console.log("Short", cantMaterias === 0 ? true : false);
+
+//Switch
+let frutaOverdura = ["Banana", "Palta", "Melon", "Manzana"];
+
+switch (frutaOverdura[0]) {
+    case "Banana": console.log("Se vende por kg");
+    break;
+    case "Palta": console.log("Se vende x u");
+    break;
+    case "Melon": console.log("Se vende x u");
+    break;
+    case "Manzana": console.log("Se vende x kg");
+    break;
+    default:  console.log("se vende x kg")
+}
+
+//For 
+
+for(let i = 0; i<frutaOverdura.length; i++){
+    console.log(frutaOverdura[i])
+}
+
+
+
+
